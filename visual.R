9983c69d09b10ae9cc631222a2b06cd1e7883233
arvindn7 <arvind.namasivayam@gmail.com>
2016-12-06 22:32:20 +1100

    Add files via upload

--- visual.R
+++ visual.R
@@ -2,5 +2,10 @@ library(ggplot2)
 
 completedResults <- read.csv("/Users/arvindn/bin/FinishedFixtures.csv", header = T)
 p <- ggplot(data = completedResults, aes(x=date, ReturnOn100QuidBet))
-p + xlab("Fixture Date") + ylab("Return on 100 pound bet") + 
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + geom_col()
+p + xlab("Fixture Date") + 
+  ylab("Return on Bet") + 
+  labs(title = "Betting on Manchester United", subtitle = "What if you bet 100 Pounds every game?") + 
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) + 
+  geom_col()
+
+  
